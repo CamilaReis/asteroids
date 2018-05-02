77560ac615c42a223b1181348c3a2ccebca266ec
Feliciano Santana <fsfelicianosantana@gmail.com>
2018-05-02 17:29:41 -0300

    Fix #1

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: "./client/index.js",
-  mode: process.env.NODE_ENV,
+  mode: "development",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "www")
